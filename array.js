5972dfbb5e446e1d861167c620cee878355fb297
putri <putrisanjiwanidewi@gmail.com>
2021-12-09 11:47:22 +0900

    update fungsi array

--- array.js
+++ array.js
@@ -45,4 +45,144 @@ function unShiftArr() {
 }
 unShiftArr()
 
+//Array Reverse
+const arr=['bandung', 'jakarta', 'medan']
+console.log(arr)
+
+function arrReverse(params) {
+    let result=params.reverse()
+    // return result
+    console.log(result)
+}
+arrReverse(arr) 
+console.log(arr)//array telah ter revese 
+
+
+//fungsi concat
+
+function arrConcat(paramsA, paramsB) {
+    let result=paramsA.concat(paramsB)
+    return result
+}
+
+const data=['jombang', 'mojokerto','blitar','tuban','pasuruan']
+
+console.log(arrConcat(arr,data))
+
+//fungsi slice Array untuk mengambil sebagian atau semua bagian array untuk di copy pada variable lain
+
+function arrSlice(params) {
+    let semua=params.slice()//meng copy semua array
+    let sebagian= params.slice(2,4)//meng copy sebagian yaitu pada index ke 2 hingga urutan ke 4 data
+    console.log(semua)
+    console.log(sebagian)
+}
+arrSlice(data)
+
+//fungsi array join dapat merubah data array menjadi string 
+
+function arrJoin(params) {
+    console.log(params)
+    const result =params.join(",")
+    return result
+}
+console.log(arrJoin(data))
+
+//fungsi split dapat mengubah sebuah string menjadi array
+
+var exsplit='coba fungsi spalit'
+
+function arrSplit(params) {
+    console.log(params)
+    const result =params.split(" ")//kondisi diperlukan untuk menentukan pemisahan kata
+    return result
+}
+console.log(arrSplit(exsplit))
+
+// fungsi IndexOf Array mencari index dari sebuah data dalam array
+
+function arrIndexOf(params, word) {
+    console.log(params)
+    const result =params.indexOf(word)//jika data tidak ada maka menghasilkan -1
+    return result
+}
+console.log(arrIndexOf(data, 'blitar'))
+
+// ForEach untuk perulangan 
+
+const caracters= ['a', 'b', 'c']
+
+function arrForEach(params) {
+    params.forEach(function (item, index, array) { //penamaan bebas namun urutan parameternya tetap yaitu 1 mewakili item kedua index dan ke 3 array
+        console.log(item)
+        console.log(index)
+        console.log(array)
+        
+    })
+}
+
+arrForEach(caracters)
+
+//fungsi map untuk perulangan
+
+// const caracters= ['a', 'b', 'c']
+
+function arrMap(params) {
+    params.map(function (item, index, array) { //penamaan bebas namun urutan parameternya tetap yaitu 1 mewakili item kedua index dan ke 3 array
+        console.log(item)
+        console.log(index)
+        console.log(array)
+        
+    })
+}
+
+arrMap(caracters)
+
+//fungsi Array Filter
+
+let Identitas=[
+    {
+        name:'cindy',
+        gender:'female'
+    },
+    {
+        name:'adi',
+        gender:'male'
+    },
+    {
+        name:'budi',
+        gender:'male'
+    },
+    {
+        name:'kekei',
+        gender:'female'
+    }
+]
+
+
+let female=  Identitas.filter((data)=>{
+        return data.gender==='male';
+    })
+    
+
+console.log(female)
+
+function arrFilter(params,value) {
+    const result =params.filter((data)=>{
+        return data.gender === value;
+    })
+    return result
+}
+console.log(arrFilter(Identitas,'female'))
+
+
+// fungsi array every untuk menentukan sebuah data dalam sebuah array memenuhi kriteria tertentu
+// namun hanya akan mereturn true jika semua data memenuhi kriteria
+
+console.log(Identitas.every(params=>params.gender==='male')) // mereturn false karena terdapat gender lain yaitu female
+
+//fungsi array some mirip dengan every
+//namun fungsi ini akan mereturn true jikan salah satu data ada yang memenuhi kriteria yang ditentukan
+
+console.log(Identitas.some(params=>params.gender==='male'))//return true
 
